c05660835fc7d13cd91365a154c43ac79334c512
Jarrod Baker <jarrod.bakeriii@gmail.com>
2022-05-13 10:10:12 +0100

    Setting up dependencies

---
+++ project/Dependencies.scala
@@ -0,0 +1,41 @@
+import sbt._
+
+object Dependencies {
+  lazy val sparkVersion = "3.2.1"
+  lazy val testVersion = "3.2.11"
+
+  lazy val dependencies = Seq(
+    configDeps, 
+    loggingDeps,
+    sparkDeps,
+    testingDeps,
+    gcpDeps
+  ).flatten
+
+  lazy val configDeps = Seq(
+    "com.github.pureconfig" %% "pureconfig" % "0.14.1"
+  )
+
+  lazy val loggingDeps = Seq(
+    "ch.qos.logback" % "logback-classic" % "1.2.3",
+    "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
+  )
+
+  lazy val sparkDeps = Seq(
+    "org.apache.spark" %% "spark-core" % sparkVersion % "provided",
+    "org.apache.spark" %% "spark-sql" % sparkVersion % "provided"
+  )
+
+  lazy val testingDeps = Seq(
+    "org.scalactic" %% "scalactic" % testVersion,
+    "org.scalatest" %% "scalatest" % testVersion % "test",
+    "org.scalacheck" %% "scalacheck" % "1.14.3"
+  )
+
+
+  lazy val gcpDeps = Seq(
+    "com.google.cloud" % "google-cloud-dataproc" % "2.3.2" % "provided",
+    "com.google.cloud" % "google-cloud-storage" % "2.4.2"
+  )
+
+}
---
+++ src/main/scala/io/opentargets/spark_converter/Main.scala
@@ -0,0 +1,6 @@
+package io.opentargets.spark_converter
+
+object Main extends App {
+  println("Hello from new main")
+
+}
